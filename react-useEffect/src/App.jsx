3b09428b69fd70a1538ef75a7c3f7e312ec8ae65
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-08-13 13:16:13 +0900

    React 생명 주기 함수 - MouseFollwer 컴포넌트  추가, AlertTimer 컴포넌트 추가

--- react-useEffect/src/App.jsx
+++ react-useEffect/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     fetch("http://localhost:3000/data")
       .then((res) => res.json())
       .then((res) => setData(res));
-  },[]);
+  }, []);
 
   return (
     <>
@@ -16,8 +16,57 @@ function App() {
       {data.map((el) => (
         <div key={el.id}>{el.content}</div>
       ))}
+      {/* <MouseFollower /> */}
+      <AlertTimer/>
     </>
   );
 }
+
+const MouseFollower = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+      console.log("mouseMoved!");
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  });
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: position.y,
+        left: position.x,
+        width: "20px",
+        height: "20px",
+        borderRadius: "50%",
+        backgroundColor: "purple",
+        transform: "translate(-50%,-50%)",
+      }}
+    ></div>
+  );
+};
+
+const AlertTimer = () => {
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    const setTimeId = setTimeout(() => {
+      console.log(showAlert);
+      showAlert === true ? alert("시간 초과") : null;
+    }, 3000);
+    return () => {
+      clearTimeout(setTimeId);
+    };
+  });
+
+  return <button onClick={()=>setShowAlert(false)}>빨리 클릭</button>
+
+};
 
 export default App;
